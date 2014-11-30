5e2c983c417229e8b34e299ee83598044a1b6acd
Chiwan Park <chiwanpark@icloud.com>
2014-11-30 19:59:09 +0900

    Modify chart font

--- src/main/java/com/chiwanpark/woo/view/TimeSeriesChartView.java
+++ src/main/java/com/chiwanpark/woo/view/TimeSeriesChartView.java
@@ -5,6 +5,8 @@ import com.chiwanpark.woo.model.TimeSeriesDatum;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.time.Second;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
@@ -14,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.List;
 
 @Component
@@ -54,8 +57,37 @@ public class TimeSeriesChartView extends JInternalFrame {
     ChartPanel pnChart = new ChartPanel(chart, true);
     pnChart.setPopupMenu(null);
 
-    LOG.info("Chart Font: " + chart.getTitle().getFont().getFontName());
+    LOG.info("Default chart font: " + chart.getTitle().getFont().getFontName());
+
+    setChartFont(chart);
+
+    LOG.info("Changed chart font: " + chart.getTitle().getFont().getFontName());
 
     return pnChart;
+  }
+
+  private void setChartFont(JFreeChart chart) {
+    XYPlot plot = chart.getXYPlot();
+    ValueAxis domainAxis = plot.getDomainAxis();
+    ValueAxis rangeAxis = plot.getRangeAxis();
+    Font font;
+
+    font = chart.getTitle().getFont();
+    chart.getTitle().setFont(new Font(Font.SANS_SERIF, font.getStyle(), font.getSize()));
+
+    font = chart.getLegend().getItemFont();
+    chart.getLegend().setItemFont(new Font(Font.SANS_SERIF, font.getStyle(), font.getSize()));
+
+    font = domainAxis.getLabelFont();
+    domainAxis.setLabelFont(new Font(Font.SANS_SERIF, font.getStyle(), font.getSize()));
+
+    font = domainAxis.getTickLabelFont();
+    domainAxis.setTickLabelFont(new Font(Font.SANS_SERIF, font.getStyle(), font.getSize()));
+
+    font = rangeAxis.getLabelFont();
+    rangeAxis.setLabelFont(new Font(Font.SANS_SERIF, font.getStyle(), font.getSize()));
+
+    font = rangeAxis.getTickLabelFont();
+    rangeAxis.setTickLabelFont(new Font(Font.SANS_SERIF, font.getStyle(), font.getSize()));
   }
 }
