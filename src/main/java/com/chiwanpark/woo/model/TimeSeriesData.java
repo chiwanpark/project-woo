4d9ee360a74033b3a7debdc51651ea80cf941785
Sunjoo Cho <kebi4414@hanmail.net>
2014-12-13 18:21:32 +0900

    Add analysis algorithm

--- src/main/java/com/chiwanpark/woo/model/TimeSeriesData.java
+++ src/main/java/com/chiwanpark/woo/model/TimeSeriesData.java
@@ -106,6 +106,93 @@ public class TimeSeriesData extends HashMap<Date, Double> {
     return result;
   }
 
+  public TimeSeriesData between2sigma() {
+    //2시그마 이내의 데이터값
+    TimeSeriesData data = new TimeSeriesData("Custom Analysis");
+
+    double average = getAverage();
+    double sigma = getStdDeviation();
+
+    for (Date date : keySet()) {
+      double v = get(date);
+      if (average - 2 * sigma <= v && v <= average + 2 * sigma) {
+        data.put(date, v);
+      }
+    }
+
+    return data;
+  }
+
+  public TimeSeriesData above2simga() {
+    //2시그마를 넘는 데이터값
+    TimeSeriesData data = new TimeSeriesData("Custom Analysis");
+
+    double average = getAverage();
+    double sigma = getStdDeviation();
+
+    for (Date date : keySet()) {
+      double above2s = get(date);
+      if (average - 2 * sigma < above2s && above2s > average + 2 * sigma) {
+        data.put(date, above2s);
+      }
+    }
+
+    return data;
+  }
+
+  public TimeSeriesData between5sigma() {
+    //5시그마 이내의 데이터값
+    TimeSeriesData data = new TimeSeriesData("Custom Analysis");
+
+    double average = getAverage();
+    double sigma = getStdDeviation();
+
+    for (Date date : keySet()) {
+      double above5s = get(date);
+      if (average - 5 * sigma <= above5s && above5s <= average + 5 * sigma) {
+        data.put(date, above5s);
+      }
+    }
+
+    return data;
+  }
+
+  public TimeSeriesData above5simga() {
+    //5시그마를 넘어가는 데이터값
+    TimeSeriesData data = new TimeSeriesData("Custom Analysis");
+
+    double average = getAverage();
+    double sigma = getStdDeviation();
+
+    for (Date date : keySet()) {
+      double above5s = get(date);
+      if (average - 5 * sigma < above5s && above5s > average + 5 * sigma) {
+        data.put(date, above5s);
+      }
+    }
+
+    return data;
+  }
+
+  public TimeSeriesData MovingAverage3day(){
+    // 3일치 이동평균
+    TimeSeriesData data = new TimeSeriesData("Custom Analysis");
+
+    Calendar calendar = Calendar.getInstance();
+    for (Date date : keySet()) {
+      calendar.setTime(date);
+      calendar.add(Calendar.DAY_OF_WEEK, 3);
+
+      Date endDate = calendar.getTime();
+
+      double v = filterByDate(date, endDate).getAverage();
+      data.put(date, v);
+    }
+
+    return data;
+
+  }
+
   public TimeSeriesData customAnalysis() {
     TimeSeriesData data = new TimeSeriesData("Custom Analysis");
 
