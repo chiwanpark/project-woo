e203a12e76eda61efaf40f7df4c12475da709d99
Chiwan Park <chiwanpark@icloud.com>
2014-12-13 15:01:11 +0900

    Add save graph feature

--- src/main/java/com/chiwanpark/woo/view/TimeSeriesChartView.java
+++ src/main/java/com/chiwanpark/woo/view/TimeSeriesChartView.java
@@ -13,23 +13,40 @@ import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.xy.XYDataset;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.List;
 
 @Component
 public class TimeSeriesChartView extends JInternalFrame {
   private static final Logger LOG = LoggerFactory.getLogger(TimeSeriesChartView.class);
 
+  private @Autowired MainWindow mainWindow;
+
+  private JPanel pnContents;
+  private JButton btnSaveGraph;
+  private JPanel pnChartWrap;
+
+  private ChartPanel pnChart;
+
   public TimeSeriesChartView(String title, List<TimeSeriesData> dataset) {
     super("Graph - " + title);
 
     XYDataset jfreeDataset = createDataSet(dataset);
-    ChartPanel pnChart = createChart(title, jfreeDataset);
+    pnChart = createChart(title, jfreeDataset);
 
-    setContentPane(pnChart);
+    pnChartWrap.setLayout(new BoxLayout(pnChartWrap, BoxLayout.PAGE_AXIS));
+    pnChartWrap.add(pnChart);
+
+    addSaveGraphAction();
+
+    setContentPane(pnContents);
     setSize(640, 320);
     setMaximizable(true);
     setVisible(true);
@@ -89,5 +106,19 @@ public class TimeSeriesChartView extends JInternalFrame {
 
     font = rangeAxis.getTickLabelFont();
     rangeAxis.setTickLabelFont(new Font(Font.SANS_SERIF, font.getStyle(), font.getSize()));
+  }
+
+  private void addSaveGraphAction() {
+    btnSaveGraph.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        try {
+          pnChart.doSaveAs();
+        } catch (IOException ex) {
+          ex.printStackTrace();
+          JOptionPane.showMessageDialog(mainWindow, "그래프를 저장하는데 실패했습니다!", "오류!", JOptionPane.ERROR_MESSAGE);
+        }
+      }
+    });
   }
 }
